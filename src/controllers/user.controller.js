6b6f7f049f4bad537648eaa19e56ba9b5e950ff1
Himanshi Sharma <himanshish456@gmail.com>
2024-03-17 15:12:25 +0530

    register user

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,55 @@
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+  console.log("email:", email);
+
+  if ([username, email, password].some((field) => field?.trim() === "")) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const existedUser = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (existedUser) {
+    throw new ApiError(409, "User with email or username already exists");
+  }
+
+  const profileImagePath = req.files?.profileImage[0].path;
+
+  if (!profileImagePath) {
+    throw new ApiError(400, "Profile Image is required");
+  }
+
+  const profileImage = await uploadOnCloudinary(profileImagePath);
+
+  if (!profileImage) {
+    throw new ApiError(400, "avatar file is required");
+  }
+
+  const user = await User.create({
+    username: username,
+    profileImage: profileImage.url,
+    email,
+    password,
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong while registering a user");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User registered successfully"));
+});
+
+export { registerUser };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-const app = express();
 dotenv.config({
   path: "./env",
 });
-
 connectDB()
   .then(() => {
     app.on("error", (error) => {
